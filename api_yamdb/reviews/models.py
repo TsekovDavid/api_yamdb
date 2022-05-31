eff3a7e60e2760392d0afd160247de50132e6ead
Tsekov David <tsekov.david@yandex.ru>
2022-05-31 21:41:40 +0300

    minor fixes

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,12 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     """Расширяет пользовательскую модель полями bio и role"""
+
     ROLES = [
         ('user', 'Аутентифицированный пользователь'),
         ('moderator', 'Модератор'),
         ('admin', 'Администратор'),
     ]
 
-    email = models.EmailField
+    email = models.EmailField('Почта', unique=True)
     bio = models.TextField('Биография', blank=True,)
-    role = models.CharField(max_length=20, choices=ROLES)
+    role = models.CharField(
+        'Роль', max_length=9, choices=ROLES, default='user'
+        )
