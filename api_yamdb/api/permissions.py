e893cfebf8b96c57838fd4b9f3b2e1cb3cea17bf
Tsekov David <tsekov.david@yandex.ru>
2022-06-14 14:52:39 +0300

    переделка после ревью

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,46 +2,34 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.is_admin or request.user.is_staff
+        return request.user.is_authenticated and request.user.is_admin
 
     def has_object_permission(self, request, view, obj):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.is_admin or request.user.is_staff
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsModerator(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and request.user.is_moderator
-            or request.user.is_staff
-        )
+        return request.user.is_authenticated and request.user.is_moderator
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.user.is_authenticated and request.user.is_moderator
-            or request.user.is_staff
-        )
+        return request.user.is_authenticated and request.user.is_moderator
 
 
 class IsAuthor(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_user
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.author
 
 
 class ReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
     def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS
+
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -36,15 +36,15 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-        return self.role == self.ADMIN
+        return (self.role == self.ADMIN or self.is_staff)
 
     @property
     def is_moderator(self):
-        return self.role == self.MODER
+        return (self.role == self.MODER or self.is_staff)
 
-    @property
-    def is_user(self):
-        return self.role == self.USER
+    # @property
+    # def is_user(self):
+    #     return self.role == self.USER
 
     def __str__(self):
         return self.username[:20]
