2aec09b639edb81be2147e2436c79eb96ae39dbe
Tsekov David <tsekov.david@yandex.ru>
2022-06-15 20:22:03 +0300

    create loading func for database

---
+++ api_yamdb/reviews/management/commands/load_to_database.py
@@ -0,0 +1,32 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from reviews.models import (Category, Comment, Genre, GenreTitle, Review,
+                            Title, User)
+
+DICT = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+    GenreTitle: 'genre_title.csv'
+}
+
+
+class Command(BaseCommand):
+    help = 'Загрузка данных из CSV файлов...'
+
+    def handle(self, *args, **kwargs):
+        for model, base in DICT.items():
+            with open(
+                f'{settings.BASE_DIR}/static/data/{base}',
+                'r', encoding='utf-8'
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.SUCCESS('Данные успешно загружены'))
